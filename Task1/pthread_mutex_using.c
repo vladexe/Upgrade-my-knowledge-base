8a8d2d3ba12d09fb15c7bc8a5efc70011df1062c
vladexe <v.andrishko.v333@gmail.com>
2025-01-21 22:24:55 +0200

    Implement Multithreaded Mail Server with Mutex Synchronization
    - Created a multithreaded program to simulate a mail server.
    
    - Demonstrated race conditions when accessing a shared resource
    (mail) without synchronization.
    
    - Used `pthread_mutex_t` to synchronize access to the shared
    variable:
      - Ensured correct increments using `pthread_mutex_lock` and
    `pthread_mutex_unlock`.
      - Achieved consistent results across multiple threads.
    
    - Properly initialized and destroyed the mutex to manage its
    lifecycle.
    
    - Observed the importance of synchronization for shared data in
    multithreading.
    
    Signed-off-by: vladexe <v.andrishko.v333@gmail.com>

---
+++ Task1/pthread_mutex_using.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<pthread.h>
+
+int mail = 0;
+pthread_mutex_t mutex;
+
+
+//in that case without mutex I will recieve diferent results each time
+//	./pthread_example 
+//	Result amount of mails: 16150
+//
+//	./pthread_example 
+//	Result amount of mails: 19080
+//		
+
+
+void* mail_server()
+{
+	printf("Thread %ld started\n", pthread_self());
+
+	for(int i = 0; i < 10000; i++){
+		//with lock in this cycle 
+		// 	
+		// 	./pthread_example 
+		// 	Result amount of mails: 40000
+		// We will recieve correct values 	
+		pthread_mutex_lock(&mutex);
+		mail++;
+		pthread_mutex_unlock(&mutex);
+	}
+	return NULL;
+}
+
+int main(void){
+
+	pthread_t t1,t2,t3,t4;
+	
+	// we need to init  mutex before  creating threads
+	pthread_mutex_init(&mutex, NULL);
+
+	if (pthread_create(&t1, NULL, &mail_server, NULL)) {
+		perror("Issue during create thread 1");
+		return 1;
+	}
+	if (pthread_create(&t2, NULL, &mail_server, NULL)) {
+                perror("Issue during create thread 2");
+                return 1;
+        }
+	if (pthread_create(&t3, NULL, &mail_server, NULL)) {
+                perror("Issue during create thread 3");
+                return 1;
+        }
+	if (pthread_create(&t4, NULL, &mail_server, NULL)) {
+                perror("Issue during create thread 4");
+                return 1;
+        }
+	if (pthread_join(t1, NULL)) {
+                perror("Issue during create thread 1");
+                return 1;
+        }
+        if (pthread_join(t2, NULL)) {
+                perror("Issue during create thread 2");
+                return 1;
+        }
+        if (pthread_join(t3, NULL)) {
+                perror("Issue during create thread 3");
+                return 1;
+        }
+        if (pthread_join(t4, NULL)) {
+                perror("Issue during create thread 4");
+                return 1;
+        }
+
+	// we need to destroy mutex after joining all threads
+	pthread_mutex_destroy(&mutex);
+	printf("\nResult amount of mails: %d\n", mail);
+	
+
+	return (0);
+}

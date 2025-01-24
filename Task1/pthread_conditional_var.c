71cb72a32d2636e0b81df1f944ce39f94744885b
vladexe <v.andrishko.v333@gmail.com>
2025-01-24 23:55:25 +0200

    Enhance Fuel Station Simulation with Multiple Threads
    - Implemented a multithreaded program with:
      - Two threads for fuel filling.
      - Five threads representing cars waiting for fuel.
    
    - Used `pthread_cond_broadcast` to signal all waiting cars when fuel is added.
    - Ensured thread-safe synchronization with `pthread_mutex_t` and `pthread_cond_t`.
    - Improved clarity in logs to distinguish between fueling and car threads.
    
    Signed-off-by: vladexe <v.andrishko.v333@gmail.com>

--- Task1/pthread_conditional_var.c
+++ Task1/pthread_conditional_var.c
@@ -2,7 +2,7 @@
 #include<unistd.h>
 #include<pthread.h>
 
-#define NUMBER_OF_THREADS 2
+#define NUMBER_OF_THREADS 7
 
 pthread_mutex_t mutexFuel;
 pthread_cond_t condFuel;
@@ -11,24 +11,37 @@ int fuel_amount = 0;
 
 void* filling_fuel(){
 
-    for(int i =  0; i < 5; i++){
+    for(int i =  0; i < 10; i++){
+        // we lock the mutex when we changed the global variable
         pthread_mutex_lock(&mutexFuel);
+
         fuel_amount += 15;
-        pthread_mutex_unlock(&mutexFuel);
-        pthread_cond_signal(&condFuel);
         printf("\nFilling the OKKO with A95 + 15");
         printf("\nCurrent balance A95: %d", fuel_amount);
+        //and unlock after successfully changing
+
+        pthread_mutex_unlock(&mutexFuel);
+
+        // in previous version we sent the signal only to one thread
+        // as we have only 1 car, but if we want to fill more than 1 car
+        // we need to broadcast this signal
+        pthread_cond_broadcast(&condFuel);
         sleep(1);
     }
 }
 
 void* car(){
 
+    //we lock the mutex before fueling the car
     pthread_mutex_lock(&mutexFuel);
 
     while (fuel_amount < 40){
-        printf("\n\nCar OUTPUT: No FUEL!!!\n");
+        printf("\nCar OUTPUT: No FUEL!!!");
         pthread_cond_wait(&condFuel, &mutexFuel);
+        //and what we done here
+        //     we unlock our mutex
+        //     wait for signal
+        //     lock again
     }
     fuel_amount -= 40;
     printf("\n\nGetting the A95 - 40");
@@ -43,16 +56,16 @@ int main(void){
     pthread_mutex_init(&mutexFuel, NULL);
     pthread_cond_init(&condFuel, NULL);
 
-    for (int i = 0; i < 2; i++){
-        if (i == 0){
-            if (pthread_create(&t[i],NULL, &car, NULL))
-                perror("Something wrong with thread creation");
+    for (int i = 0; i < NUMBER_OF_THREADS; i++){
+        if (i == 0 || i == 1){
+            if (pthread_create(&t[i],NULL, &filling_fuel, NULL))
+                perror("Something wrong with fuel thread creation");
         } 
-        else if (pthread_create(&t[i], NULL, &filling_fuel, NULL)){
-            perror("Something wrong  with thread creation");
+        else if (pthread_create(&t[i], NULL, &car, NULL)){
+            perror("Something wrong  with car thread creation");
         }
     }
-    for (int i = 0; i < 2; i++){
+    for (int i = 0; i < NUMBER_OF_THREADS; i++){
         if (pthread_join(t[i], NULL)){
             perror("Something wrong with thread creation");
         }   
